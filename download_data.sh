ee341b73a058bb300dbab4f607b3d6010c04ea0a
tokxinyi <tokxinyi@gmail.com>
2023-02-15 22:02:00 +0800

    20230215 - docker-compose.yml: Used docker volume instead of mapping to local - download_data.sh: script used to download dataset - hello-world.ipynb: Exploratory script for Spark

---
+++ download_data.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# options: yellow, green, fhv
+TAXI_TYPE=$1
+# e.g. 2020, 2021
+YEAR=$2
+
+URL_PREFIX="https://d37ci6vzurychx.cloudfront.net/trip-data"
+
+for MONTH in {1 .. 12}; do
+    FMONTH=`printf "%02d" ${MONTH}`
+
+    URL="${URL_PREFIX}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.parquet"
+
+    LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
+    LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.parquet"
+    LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
+
+    echo "downloading ${URL} to ${LOCAL_PATH}"
+    mkdir -p ${LOCAL_PREFIX}
+    wget ${URL} -O ${LOCAL_PATH}
+
+done
